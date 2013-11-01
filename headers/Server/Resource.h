829d1f6f3077d892a2432245476db88ff7d84a58
marggi-s <stevy.marggi@epitech.eu>
2013-11-01 22:12:42 +0100

    ajoute de la class resource

---
+++ headers/Server/Resource.h
@@ -0,0 +1,27 @@
+#pragma once
+#include	<iostream>
+#include	<string>
+#include	<list>
+#include	<array>
+#include	"Client.h"
+#include	"Room.h"
+#include	"MetaSocket.h"
+/*#include	"MateThread.h"*/
+
+class Resource
+{
+	std::list<Client *>	_clients;
+	std::list<Room *>	_rooms;
+/*	std::list<Game *>	_games;*/
+	std::array<bool, 255>	_idRooms;
+public:
+	Resource();
+	~Resource();
+/*	std::list<Game *> const &getGame();*/
+	void		addClient(int, MetaSocket<> *);
+	std::list<Client *> const	&getClients();
+	void		createRoom(Client *);
+	std::list<Room *> const		&getRooms();
+	void		createGame(int);
+};
+
---
+++ sources/Server/Resource.cpp
@@ -0,0 +1,59 @@
+#include "Resource.h"
+
+
+Resource::Resource()
+{
+	for (int i = 0; i < 255; ++i)
+		this->_idRooms[i] = false; 
+}
+
+
+Resource::~Resource() {}
+
+void		Resource::addClient(int id, MetaSocket<> *socket)
+{
+	this->_clients.push_back(new Client(id, socket));
+}
+
+std::list<Client *> const	&Resource::getClients()
+{
+	return (this->_clients);
+}
+
+void		Resource::createRoom(Client *client)
+{
+	int		id = -1;
+
+	for (int i = 0; i < 255 && id != -1; ++i)
+		if (!this->_idRooms[i])
+		{
+			id = i;
+			this->_idRooms[i] = true;
+		}
+	this->_rooms.push_back(new Room(client->getID(), client));
+}
+
+std::list<Room *> const		&Resource::getRooms()
+{
+	return (this->_rooms);
+}
+
+void		Resource::createGame(int idClient)
+{
+	int		find = false;
+
+	for (std::list<Room *>::iterator it = this->_rooms.begin();
+			it != this->_rooms.end() && find == false; ++it)
+		if ((*it)->getHost()->getID() == idClient)
+			{
+				it = this->_rooms.erase(it);
+				find = true;
+			}
+	/*cretae thread + rajoute dans la list des game*/
+}
+
+/*std::list<Game *> const &Resource::getGame()
+{
+	return (this->_games);
+}*/
+

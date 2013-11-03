5977be046f3dffb98a7024b458a4790bc05923f6
suchet-q <quentin.suchet@epitech.eu>
2013-11-03 04:42:40 +0100

    zizi commit

--- headers/Server/Server.h
+++ headers/Server/Server.h
@@ -1,22 +1,24 @@
 #pragma once
 
-# include		<sys/select.h>
-# include		<iostream>
-# include		<string>
-# include		<list>
-# include		<array>
+#include		<sys/select.h>
+#include		<iostream>
+#include		<string>
+#include		<list>
+#include		<array>
 
-# include		"Client.h"
-# include		"Network.h"
-# include		"Resource.h"
-# include		"StructCommands.h"
+#include		"Client.h"
+#include		"Network.h"
+#include		"Resource.h"
+#include		"StructCommands.h"
+#include		"Executer.h"
 
 class			Server
 {
 private:
   Network		_network;
   Resource		_resources;
-
+  Executer		_executer;
+  
 public:
   Server();
   ~Server();
